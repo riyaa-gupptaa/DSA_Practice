b1e62185ccb1877de92f8055e6021cf3f0d895a1
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-06-22 11:36:29 +0530

    Add files via upload

---
+++ Strings/Determine if String Halves are alike.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    bool isVowel(char &ch)
+    {
+        return (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
+    }
+    bool halvesAreAlike(string s)
+    {
+        int n=s.length();
+        int mid=n/2;
+        int i=0;
+        int j=mid;
+        int countL=0;
+        int countR=0;
+        while(i<n/2 && j<n)
+        {
+            if(isVowel(s[i]))
+            {
+                countL++;
+            }
+            if(isVowel(s[j]))
+            {
+                countR++;
+            }
+            i++;
+            j++;
+        }
+        return countL==countR;
+    }
+};
