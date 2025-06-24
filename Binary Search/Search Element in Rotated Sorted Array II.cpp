19b186a9e1921aead70df4174076571b3dda475b
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-06-24 15:05:56 +0530

    Add files via upload

---
+++ Binary Search/Search Element in Rotated Sorted Array II.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool search(vector<int>& nums, int target)
+{
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)
+        {
+            return true;
+        }
+        if(nums[low]==nums[mid] && nums[mid]==nums[high])
+        {
+            low=low+1;
+            high=high-1;
+            continue;
+        }
+        if(nums[low]<=nums[mid])
+        {
+            if(nums[low]<=target && target<=nums[mid])
+            {
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(nums[mid]<=target && target<=nums[high])
+            {
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return false;
+}
+//Time complexity is O(logn)-Average case
+//For worst case O(n/2);
