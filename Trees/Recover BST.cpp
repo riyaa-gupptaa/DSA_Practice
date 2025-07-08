a85b37cbe4db24253d70bba19316b105de7120f6
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-07-08 22:37:59 +0530

    Add files via upload

---
+++ Trees/Recover BST.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+void recoverTree(TreeNode *root)
+{
+
+}
+//Time complexity is O(N)
+//Space complexity is O(1)
