435082144cea8cec81223334ada0390878582be7
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-07-11 15:43:28 +0530

    Add files via upload

---
+++ Trees/Requirements need to construct a BT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+bool isPossible(int a, int b) 
+{
+    
+}
