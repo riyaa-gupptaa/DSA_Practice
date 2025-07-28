9e0cb26fb62d8d5be78a89c4df50b35bf6c3c36d
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-07-28 21:35:34 +0530

    Add files via upload

--- Dynamic Programming/Minimum Cost to cut the stick.cpp
+++ Dynamic Programming/Minimum Cost to cut the stick.cpp
@@ -58,3 +58,31 @@ int minCost(int n, vector<int>& cuts)
     vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
     return f(1,c,cuts,dp);
 }
+
+//Tabulation
+int minCost(int n, vector<int>& cuts)
+{
+    int c=cuts.size();
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(),0);
+    sort(cuts.begin(),cuts.end());
+    vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+    for(int i=c;i>=1;i--)
+    {
+        for(int j=1;j<=c;j++)
+        {
+            if(i>j)
+            {
+                continue;
+            }
+            int mini=INT_MAX;
+            for(int ind=i;ind<=j;ind++)
+            {
+                int cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                mini=min(mini,cost);
+            }
+            dp[i][j]=mini;
+        }
+    }
+    return dp[1][c];
+}
