b9f3a096b5245145632b1c3be0a01792acf58f95
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-07-28 21:21:07 +0530

    Add files via upload

---
+++ Dynamic Programming/Minimum Cost to cut the stick.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+int f(int i,int j,vector<int> &cuts)
+{
+    if(i>j)
+    {
+        return 0;
+    }
+    int mini=INT_MAX;
+    for(int ind=i;ind<=j;ind++)
+    {
+        int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts)+f(ind+1,j,cuts);
+        mini=min(mini,cost);
+    }
+    return mini;
+}
+int minCost(int n, vector<int>& cuts)
+{
+    int c=cuts.size();
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(),0);
+    sort(cuts.begin(),cuts.end());
+    return f(1,c,cuts);
+}
+//Exponential time complexity
+
+//Memoization
+int f(int i,int j,vector<int> &cuts,vector<vector<int>> &dp)
+{
+    if(i>j)
+    {
+        return 0;
+    }
+    if(dp[i][j]!=-1)
+    {
+        return dp[i][j];
+    }
+    int mini=INT_MAX;
+    for(int ind=i;ind<=j;ind++)
+    {
+        int cost=cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
+        mini=min(mini,cost);
+    }
+    return dp[i][j]=mini;
+}
+int minCost(int n, vector<int>& cuts)
+{
+    int c=cuts.size();
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(),0);
+    sort(cuts.begin(),cuts.end());
+    vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
+    return f(1,c,cuts,dp);
+}
