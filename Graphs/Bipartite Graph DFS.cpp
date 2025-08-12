d79dda9dd60589fed635c04b018ecd32439630a7
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-08-12 23:33:10 +0530

    Add files via upload

---
+++ Graphs/Bipartite Graph DFS.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+bool dfs(int node,int col,vector<int> &color,vector<vector<int>> &graph)
+{
+    color[node]=col;
+    for(auto it: graph[node])
+    {
+        if(color[it]==-1)
+        {
+            if(dfs(it,col,color,graph)==false);
+        }
+        else if(color[it]==col)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool isBipartite(vector<vector<int>>& graph)
+{
+    int V=graph.size();
+    vector<int> color(V,0);
+    for(int i=0;i<V;i++)
+    {
+        color[i]=-1;
+    }
+    for(int i=0;i<V;i++)
+    {
+        if(color[i]==-1)
+        {
+           if(dfs(i,0,color,graph)==false)
+           {
+            return false;
+           }
+        }
+    }
+    return true;
+}
