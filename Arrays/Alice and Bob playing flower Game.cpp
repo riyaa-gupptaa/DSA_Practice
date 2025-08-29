425eaf7e83cd6909762145a0e6d31e764042d794
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-08-29 13:22:56 +0530

    Add files via upload

---
+++ Arrays/Alice and Bob playing flower Game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        const int xEven = n / 2;
+        const int yEven = m / 2;
+        const int xOdd = (n + 1) / 2;
+        const int yOdd = (m + 1) / 2;
+        return static_cast<long>(xEven) * yOdd +
+               static_cast<long>(yEven) * xOdd;
+    }
+};
