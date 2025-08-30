7bd38504f1d7e3298596ccda0ad51037db7dcff5
Riya Gupta <148568034+riyaa-gupptaa@users.noreply.github.com>
2025-08-30 23:29:19 +0530

    Add files via upload

---
+++ Arrays/String to Integer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        while (i < n && s[i] == ' ')
+            ++i;
+
+        int sign = 1;
+        if (i < n && (s[i] == '-' || s[i] == '+')) {
+            sign = s[i] == '-' ? -1 : 1;
+            ++i;
+        }
+
+        int res = 0;
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            if (res > INT_MAX / 10 ||
+                (res == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+            res = res * 10 + digit;
+            ++i;
+        }
+        return res * sign;
+    }
+};
